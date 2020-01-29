a1cc3ccdf4be3d38f870e1916689b5e8a10d3c83
Jaxpulz <alonr1127@gmail.com>
2020-01-29 02:21:16 +0100

    commit

--- py_ping.py
+++ py_ping.py
@@ -1,3 +1,4 @@
+
 from subprocess import check_call, CalledProcessError,PIPE
 import socket
 import time
@@ -6,23 +7,33 @@ import os
 import logging
 
 
-logging.basicConfig(filename='py_ping_test.log',level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s', datefmt='%Y/%m/%d %H:%M:%S')
+
+logging.basicConfig(filename="newfile.log", 
+                    format='%(asctime)s %(message)s', 
+                    filemode='w') 
+#Creating an object 
+logger=logging.getLogger() 
+  
+#Setting the threshold of logger to DEBUG 
+logger.setLevel(logging.DEBUG) 
 
 ip = input("Lökj egy IP-t vagy host nevet pingeléshez: ")
 
 print("\nIde fog logolni: {}\py_ping_test.log".format(os.path.dirname(os.path.realpath(__file__))))
 print("Ping tesztelés fut...")
+
 while True:
     try:
-        check_call(['ping', '-n', '3', ip],stdout=PIPE)
-        logging.info('ok ping test {}'.format(ip))
+        check_call(['ping', '-n', '3', ip])
+        logger.info('ok ping test {}'.format(ip))
+    
     except CalledProcessError as e:
-        logging.error('failed ping test {}'.format(ip))
-
+        logger.error('failed ping test {}'.format(ip))
+    
     try:
         socket.gethostbyname(ip)
-        logging.info('ok DNS test {}'.format(ip))
+        logger.info('ok DNS test {}'.format(ip))
     except:
-        logging.error('failed DNS test {}'.format(ip))
+        logger.error('failed DNS test {}'.format(ip))
 
     time.sleep(3)
