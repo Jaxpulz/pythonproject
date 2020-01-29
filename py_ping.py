098f88a90c4870946c3b7a4e8a369f5f365db6cc
Jaxpulz <alonr1127@gmail.com>
2020-01-29 02:30:34 +0100

    commit

--- py_ping.py
+++ py_ping.py
@@ -6,7 +6,7 @@ import os
 import logging
 
 
-logging.basicConfig(filename='example.log',level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s', datefmt='%Y/%m/%d %H:%M:%S')
+logging.basicConfig(filename='py_ping_test.log',level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s', datefmt='%Y/%m/%d %H:%M:%S')
 def setup_logging():
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
@@ -21,7 +21,7 @@ def setup_logging():
 logger = setup_logging()
 ip = input("Lökj egy IP-t vagy host nevet pingeléshez: ")
 
-print("\nIde fog logolni: {}\py_ping_test.log".format(os.path.dirname(os.path.realpath(__file__))))
+print("\nIde fog logolni: {}/py_ping_test.log".format(os.path.dirname(os.path.realpath(__file__))))
 print("Ping tesztelés fut...")
 while True:
     try:
